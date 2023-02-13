37f8801f01cba1067a2e47f2c29433266f610641
Anton Klennitski <75129039+Warrior-RS@users.noreply.github.com>
2023-02-13 19:30:06 +0100

    Update KatyaTask.py

--- KatyaTask.py
+++ KatyaTask.py
@@ -1,3 +1,16 @@
+
+#Katya has an unlimited number of cards on which the number 1 is written.
+#She wonders if it is possible to put together a number that is divisible by N with these cards. Moreover, she is interested in the number that uses as few cards as possible. Since Katya is not good at math, she asked for your help. 
+ 
+#The format of the input data:
+#The only line of input data contains the number N (1≤N≤105). 
+ 
+#Output data format
+#In case it is impossible to make up a number, print "NO". Otherwise, output the minimum number composed of units that are divisible by the given
+
+
+
+
 def smallest_number_divisible_by_n(n):
     for i in range(1, n + 1):
         if (10 ** i - 1) % n == 0:
