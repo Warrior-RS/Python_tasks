216ed460a9f9697ca0a3cec0e153ae345d1432f0
Anton Klennitski <75129039+Warrior-RS@users.noreply.github.com>
2023-02-13 20:43:50 +0100

    Update reverse_array.py

--- reverse_array.py
+++ reverse_array.py
@@ -1,3 +1,29 @@
+#Given array [a, b, c, d, e, f, g, h, i, j, k, l, m, n, o].
+
+#You must reverse first 2 elements of the array, then 3, then 2, then 3, and so on, until the end
+
+#The result: [b, a, e, d, c, g, f, j, i, h, l, k, o, n, m]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 arr = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o"]
 
 for i in range(0, len(arr), 5):
